5dbe1e6570e09c91d65d40a11ee375ef40c12ac3
Israël TODOME <tdonanon@outlook.fr>
2025-02-18 15:07:19 -0500

    Ajoutez des fichiers projet.

---
+++ Jeu_du_Pendu/Jeu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu_du_Pendu
+{
+    public class Jeu
+    {
+        private Pendu pendu = new Pendu();
+        public void LancerJeu()
+        {
+
+        }
+    }
+}
---
+++ Jeu_du_Pendu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu_du_Pendu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Jeux = new Jeu();
+            Jeux.LancerJeu();
+        }
+    }
+}
